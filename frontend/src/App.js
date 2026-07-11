a71a5161bca92c8d565a60bf0c022ae33a280064
root <root@ip-10-20-102-155.ap-south-1.compute.internal>
2026-07-11 14:18:10 +0000

    changed frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -67,7 +67,7 @@ function App() {
   return (
     <div className="container">
 
-      <h1>Expensesssss Tracker</h1>
+      <h1>Expensesssss Tracker in dev environmnet</h1>
 
       <div className="expense-form">
 
