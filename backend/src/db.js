33a48b8e1147c4f9fb2b27e910013fae151f40b1
root <root@ip-10-20-102-155.ap-south-1.compute.internal>
2026-07-12 06:54:20 +0000

    modified db.js

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: "eks-app-rds.c5cgiws8oi9q.ap-south-1.rds.amazonaws.com",
-  user: "admin",
-  password: "admin12345",
-  database: "expense_db"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 module.exports = pool;
